f5aa36e72a34a80086afc4cbcea85420d885ac4d
DouglasPenn23 <63472155+DouglasPenn23@users.noreply.github.com>
2021-04-14 13:59:35 -0400

    Basic setup 27:52 in Video

---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student {
+
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int feesTotal;
+
+    public Student(int id, String name, int grade){
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+}

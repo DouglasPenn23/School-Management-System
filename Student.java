9329006aace75bb2d4564a45a9b2368d55ed7893
DouglasPenn23 <63472155+DouglasPenn23@users.noreply.github.com>
2021-05-17 10:50:59 -0400

    Added the Teachers Class

--- Student.java
+++ Student.java
@@ -16,8 +16,22 @@ public class Student {
 
     // Not going to alter student's name, students id
 
+    // Used to update the students grade.
+    // @Param grade new grade of the student.
+
     public void setGrade(int grade){
         this.grade=grade;
 
+    }
+
+    // This method is going to
+    // Add the fees to the fees paid field
+    // if initially 0 & they pay 10,000 the fees paid should be 10,000
+    // If they pay another 5k, they should have 15000 paid
+    // So they want a var to keep adding the fees to feespaid
+    // The school is going to receive the funds.
+
+    public void updateFeesPaid(int fees){
+        feesPaid += fees;
     }
 }
---
+++ Teacher.java
@@ -0,0 +1,28 @@
+
+// This class is responsible for keeping track of the teacher's
+// name salary & id
+
+public class Teacher {
+    private int id;
+    private String name;
+    private int salary;
+
+
+    // There will be different salaries for each teacher
+    // Therefore I need to find a way to code that.
+    public Teacher(int id, String name, int salary){
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+
+    }
+
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getId(){
+        return
+    }
+}
